6f317dd16e5c51309c090a746226485722fee870
stuiterveer <admin@stuiterveer.com>
2025-06-03 19:36:30 +0200

    Make it possible to exit the game

--- Levels/game_over.gd
+++ Levels/game_over.gd
@@ -13,5 +13,8 @@ func _on_retry_button_button_up():
 	GlobalVars.reset()
 	get_tree().change_scene_to_file("res://Levels/gameplay.tscn")
 
+func _on_exit_button_button_up():
+	get_tree().quit()
+
 func render_ui():
 	score_label.text = "Score: " + str(GlobalVars.player_score)
